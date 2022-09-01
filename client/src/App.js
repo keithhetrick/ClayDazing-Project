30b3b093ae4be4b43af4b1e721248e1d362e119e
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-08-31 23:53:28 -0500

    app.js update

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import ProductView from "./components/ProductView";
 // import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeView from "./components/HomeView";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomeView />
-      <ProductView />
-      {/* <Footer /> */}
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/products" element={<ProductView />} />
+        {/* <Footer /> */}
+      </Routes>
     </div>
   );
 }
